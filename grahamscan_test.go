80690baabe39cf6bdfe82f2f295b3cdb67eea4bf
Alexander F. Rødseth <alexander.rodseth@appeartv.com>
2019-01-02 14:32:03 +0100

    Update test

--- grahamscan_test.go
+++ grahamscan_test.go
@@ -9,6 +9,8 @@ func Example() {
 		&Point{0, 0},
 		&Point{1, 2},
 		&Point{3, 4},
+		&Point{-4, 5},
+		&Point{20, 70},
 	}
 	newPoints, err := ps.Compute()
 	if err != nil {
@@ -17,6 +19,6 @@ func Example() {
 	fmt.Println(ps)
 	fmt.Println(newPoints)
 	// Output:
-	// [{0 0} {3 4} {1 2}]
-	// [{1 2} {3 4} {0 0}]
+	// [{0 0} {3 4} {1 2} {20 70} {-4 5}]
+    // [{-4 5} {20 70} {3 4} {0 0}]
 }
